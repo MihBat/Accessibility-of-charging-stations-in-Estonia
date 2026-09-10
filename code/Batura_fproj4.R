e0670ce72491e1fe1f9599c51363c2aea292dd15
Mihhail Batura <baturamihail229@gmail.com>
2026-09-10 14:43:05 +0300

    Update Batura_fproj4.R

--- code/Batura_fproj4.R
+++ code/Batura_fproj4.R
@@ -52,14 +52,7 @@ roads_clipped <- st_intersection(roads_crop, counties_SE)
 # save and check on GIS
 st_write(roads_clipped, "C:/PythonGIS/geopython2025/R_01/roads_SE.shp", delete_layer = TRUE)
 
-
-
-# for design a final map load layers that you need to represent
-roads_se <- st_read("C:/PythonGIS/geopython2025/R_01/roads_SE.shp")
-
 travel_time_100m <- rast("C:/PythonGIS/geopython2025/R_01/travel_time_SE_counties.tif")
-
-stations_se <- st_read("C:/PythonGIS/geopython2025/R_01/stations_counties_SE.shp")
 
 # to avoid a system crash make resolution lower
 # Aggregate to 500x500 m: factor = 5 (since 500 / 100 = 5)
@@ -68,97 +61,6 @@ travel_time_500m <- aggregate(travel_time_100m, fact = 5, fun = "mean", na.rm = 
 # save and check on GIS
 writeRaster(travel_time_500m, "C:/PythonGIS/geopython2025/R_01/travel_time_500m.tif", overwrite = TRUE)
 
-# check crs
-crs_rast <- crs(travel_time_500m)
-roads_se <- st_transform(roads_se, crs_rast)
-stations_se <- st_transform(stations_se, crs_rast)
-
-# Define 6 classes of travelling time
-breaks <- c(0, 10, 20, 30, 40, 50, Inf)
-labels <- c("≤ 10", "10–20", "20–30", "30–40", "40–50", "> 50")
-
-# Classify 
-rcl_matrix <- matrix(c(
-  0, 10, 1,
-  10, 20, 2,
-  20, 30, 3,
-  30, 40, 4,
-  40, 50, 5,
-  50, Inf, 6
-), ncol = 3, byrow = TRUE)
-
-tt_classified <- classify(travel_time_500m, rcl = rcl_matrix)
-
-# Convert to df for ggplot
-tt_df <- as.data.frame(tt_classified, xy = TRUE, na.rm = TRUE)
-colnames(tt_df)[3] <- "class"
-
-# Make sure it's a factor with correct levels
-tt_df$class <- factor(tt_df$class, levels = 1:6, labels = labels)
-
-# define a color palette for 6 classes
-travel_colors <- c(
-  "≤ 10"  = "#2b83ba", 
-  "10–20" = "#71b4d9", 
-  "20–30" = "#fee08b", 
-  "30–40" = "#fca636", 
-  "40–50" = "#f58220", 
-  "> 50"  = "#EE1C00"  
-)
-
-# design a map
-ggplot() +
-  
-  geom_raster(data = tt_df, aes(x = x, y = y, fill = class)) +
-  scale_fill_manual(
-    name = "Travel time, min",
-    values = travel_colors,
-    guide = guide_legend(order = 3)  # set a position in legend, point might be first, lines - second, polygons/raster third
-  ) +
-  
-  
-  geom_sf(data = roads_se, 
-          aes(color = "Road network"),
-          size = 0.05, alpha = 1) +
-  scale_color_manual(
-    name = "", 
-    values = c("Road network" = "grey20"),
-    guide = guide_legend(order = 2)  
-  ) +
-  
-  
-  geom_sf(data = stations_se, 
-          aes(shape = "Charging station"),
-          fill = "yellow", color = "black", size = 2) +
-  scale_shape_manual(
-    name = "", 
-    values = c("Charging station" = 21),
-    guide = guide_legend(order = 1)  
-  ) +
-  
-  
-  annotation_scale(location = "bl", width_hint = 0.2) +
-  annotation_north_arrow(location = "tr", which_north = "true",
-                         style = north_arrow_orienteering(text_size = 0)) +
-  labs(
-    title = "Charging station accessibility in southeastern Estonia",
-    subtitle = "Source: OpenChargeMap, Estonian Landboard",
-    caption = "Author: Mihhail Batura",
-    x = "", # delete extra labels from figure
-    y = ""
-  ) +
-  coord_sf() +
-  theme_minimal(base_size = 12) +
-  theme(
-    plot.title = element_text(hjust = 0.5, color = "black"),
-    plot.subtitle = element_text(hjust = 0, color = "black", size = 8),
-    plot.caption = element_text(hjust = 1, color = "black", size = 8),
-    plot.background = element_rect(fill = "white"), # Set the plot background to white
-    panel.background = element_rect(fill = "white"),
-    legend.title = element_text(size = 8),
-    legend.position = "right"
-  )
+# final output visualisation in QGIS using travel_time_500m.tif, roads_SE.shp, stations_counties_SE.shp
 
 
-# Save the map as a PNG image
-ggsave("C:/PythonGIS/geopython2025/R_01/MB_travel_time_se.png", width = 8, height = 6, dpi = 300)
